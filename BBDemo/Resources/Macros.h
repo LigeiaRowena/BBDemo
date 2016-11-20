a717cea92261d0405b787668d98b198b12c4856a
Francesca Corsini <francesca.corsini@wuerth-phoenix.com>
2016-11-20 17:12:46 +0100

    Add xcode groups/folders and macro with inline functions

---
+++ BBDemo/Resources/Macros.h
@@ -0,0 +1,80 @@
+//
+//  Macros.h
+//  BBDemo
+//
+//  Created by Francesca Corsini on 20/11/16.
+//  Copyright © 2016 Francesca Corsini. All rights reserved.
+//
+
+
+#pragma mark - Interface orientation
+
+
+static inline UIInterfaceOrientation getOrientation() {
+	if ([UIDevice currentDevice].orientation == UIDeviceOrientationPortrait || [UIDevice currentDevice].orientation == UIDeviceOrientationPortraitUpsideDown)
+		return UIInterfaceOrientationPortrait;
+	else if ([UIDevice currentDevice].orientation == UIDeviceOrientationLandscapeLeft)
+		return UIInterfaceOrientationLandscapeRight;
+	else if ([UIDevice currentDevice].orientation == UIDeviceOrientationLandscapeRight)
+		return UIInterfaceOrientationLandscapeLeft;
+	else
+		return [[UIApplication sharedApplication] statusBarOrientation];
+}
+
+
+#pragma mark - Device version
+
+static inline BOOL iPad() {
+    return (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad);
+}
+
+
+static inline BOOL iPhone() {
+    return (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone);
+}
+
+
+#pragma mark - UI Utility
+
+
+static inline CGFloat textWidth(UILabel *label) {
+	return [label.text boundingRectWithSize:label.frame.size options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:label.font} context:nil].size.width;
+}
+
+
+#pragma mark - NS Utility
+
+
+static inline NSNumberFormatter *getDecimalFormatter(int digits) {
+	NSNumberFormatter *defaultFormatter = [[NSNumberFormatter alloc] init];
+	defaultFormatter.locale = [NSLocale currentLocale];
+	[defaultFormatter setFormatterBehavior:NSNumberFormatterBehaviorDefault];
+	[defaultFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+	[defaultFormatter setRoundingMode:NSNumberFormatterRoundHalfUp];
+	[defaultFormatter setMaximumFractionDigits:digits];
+	[defaultFormatter setMinimumFractionDigits:digits];
+	defaultFormatter.alwaysShowsDecimalSeparator = NO;
+	return defaultFormatter;
+}
+
+
+static inline BOOL stringContainsOccurenceOfString(NSString *string, NSString *occurence){
+	NSRange range = [string rangeOfString:occurence options:NSCaseInsensitiveSearch];
+    return (range.lenght > 0)
+}
+
+
+static inline BOOL isEmptyObject(id object) {
+    return (object == nil || [object isEqual:[NSNull null]] || [object count] == 0);
+}
+
+
+static inline BOOL isEmptyNumber(NSNumber *number) {
+    return ([number isEqualToNumber:[NSNumber numberWithInt:0]] || [number isEqual:[NSNull null]]);
+}
+
+
+static inline BOOL isEmptyString(NSString *string) {
+    return (string == nil || [string isEqual:[NSNull null]] || [[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] isEqualToString:@""])
+}
+
