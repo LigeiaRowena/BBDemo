8da05c8344604113c5083dfaad5d0cfc189f424a
Francesca Corsini <francesca.corsini@wuerth-phoenix.com>
2016-11-21 13:00:36 +0100

    Add Photo view

---
+++ BBDemo/ViewControllers/PhotoViewController.h
@@ -0,0 +1,15 @@
+//
+//  PhotoViewController.h
+//  BBDemo
+//
+//  Created by Francesca Corsini on 21/11/16.
+//  Copyright © 2016 Francesca Corsini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PhotoViewController : UIViewController
+
+
+@end
---
+++ BBDemo/ViewControllers/PhotoViewController.m
@@ -0,0 +1,28 @@
+//
+//  PhotoViewController.m
+//  BBDemo
+//
+//  Created by Francesca Corsini on 21/11/16.
+//  Copyright © 2016 Francesca Corsini. All rights reserved.
+//
+
+#import "PhotoViewController.h"
+
+
+@implementation PhotoViewController
+
+
+#pragma mark - Init/Launch ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
